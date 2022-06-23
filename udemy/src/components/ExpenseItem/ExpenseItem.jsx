e65041e1e7879ed11afb53419e8df5b2b33daa37
Charles Fisher <charles.fisher@americaneagle.com>
2022-06-23 13:46:10 -0500

    Refactor app with expense component

--- udemy/src/components/ExpenseItem/ExpenseItem.jsx
+++ udemy/src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,15 +1,17 @@
 import './ExpenseItem.css'
+import ExpenseDate from '../ExpenseDate/ExpenseDate';
 
 function ExpenseItem(props) {
     
     return (
         <>
             <section className='expense-item' aria-label='Expense Item'>
-                <div>{props.date.toLocaleString()}</div>
+                <ExpenseDate date={props.date}/>
                 <div className='expense-item__description'>
                     <h2>{props.title}</h2>
                     <div className='expense-item__price'>${props.amount}</div>
                 </div>
+
             </section>
         </>
     )
