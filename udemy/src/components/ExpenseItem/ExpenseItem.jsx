d21796f446e016e75c4f8ec9713599c73a95e2f2
Charles Fisher <charles.fisher@americaneagle.com>
2022-07-18 11:10:03 -0500

    Add useState hook.

--- udemy/src/components/ExpenseItem/ExpenseItem.jsx
+++ udemy/src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,20 +1,30 @@
+import React, { useState } from 'react';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import Card from '../Card/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+
   const titleSplice = props.title.replaceAll(' ', '-');
+ 
+  const buttonClickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+  } 
+
   return (
     <>
-      <Card className='expense-item' aria-labelledby={titleSplice}>
+      <Card className='expense-item'>
         <ExpenseDate date={props.date} />
-        <div className='expense-item__description'>
-          <h2 id={titleSplice}>{props.title}</h2>
+        <div className='expense-item__description' aria-labelledby={titleSplice}>
+          <h2 id={titleSplice}>{title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
         </div>
+        <button onClick={buttonClickHandler}>Change Title</button>
       </Card>
     </>
-   
   );
 }
 
