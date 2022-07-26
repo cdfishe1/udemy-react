56a0751e8a095e58fc41fa2b9522ab66bec43ef1
Charles Fisher <charles.fisher@americaneagle.com>
2022-07-26 09:44:44 -0500

    Expense form and new expense components.

---
+++ udemy/src/components/ExpenseForm/ExpenseForm.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
+
+    const titleChangeHandler = event => {
+        setTitle(event.target.value);
+        console.log(event.target.value);
+    }
+
+    const amountChangeHandler = event => {
+        setAmount(event.target.value);
+        console.log(event.target.value);
+    }
+
+    const dateChangeHandler = event => {
+        setDate(event.target.value);
+        console.log(event.target.value);
+    }
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: title,
+            amount: amount,
+            date: new Date(date)
+        }
+
+        props.onSaveExpenseData(expenseData);
+        setTitle('');
+        setAmount('');
+        setDate('');
+    }
+
+    return (
+        <>
+            <form onSubmit={submitHandler}>
+                <div className='new-expense__controls'>
+                    <div className='new-expense__control'>
+                        <label htmlFor="title">Title</label>
+                        <input id='title' type="text" value={title} onChange={titleChangeHandler} placeholder="expense" />
+                    </div>
+
+                    <div className='new-expense__control'>
+                        <label htmlFor="amount">Amount</label>
+                        <input id='amount' type="number" min='0.01' step='0.01' value={amount} onChange={amountChangeHandler} placeholder="1.00" />
+                    </div>
+
+                    <div className='new-expense__control'>
+                        <label htmlFor="date">Date</label>
+                        <input id='date' type="date" min='2019-01-01' max='2022-12-31' value={date} onChange={dateChangeHandler} />
+                    </div>
+
+                    <div className='new-expense__actions'>
+                        <button type='submit'>Add Expense</button>
+                    </div>
+                </div>  
+            </form>
+        </>
+    )
+}
+
+export default ExpenseForm;
--- udemy/src/components/ExpenseItem/ExpenseItem.jsx
+++ udemy/src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import Card from '../Card/Card';
 import './ExpenseItem.css';
@@ -7,8 +7,6 @@ function ExpenseItem(props) {
   const [title, setTitle] = useState(props.title);
   console.log('ExpenseItem evaluated by React');
 
-  const titleSplice = props.title.replaceAll(' ', '-');
- 
   const buttonClickHandler = () => {
     setTitle('Updated!');
     console.log(title);
@@ -18,8 +16,8 @@ function ExpenseItem(props) {
     <>
       <Card className='expense-item'>
         <ExpenseDate date={props.date} />
-        <div className='expense-item__description' aria-labelledby={titleSplice}>
-          <h2 id={titleSplice}>{title}</h2>
+        <div className='expense-item__description'>
+          <h2>{title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
         </div>
         <button onClick={buttonClickHandler}>Change Title</button>
