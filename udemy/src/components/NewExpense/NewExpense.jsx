ed34a191a769e7e76deb41b49fc85d68e7625af9
Charles Fisher <charles.fisher@americaneagle.com>
2022-07-29 08:37:20 -0500

    Refactor uniquie id.

--- udemy/src/components/NewExpense/NewExpense.jsx
+++ udemy/src/components/NewExpense/NewExpense.jsx
@@ -1,11 +1,13 @@
 import './NewExpense.css';
 import ExpenseForm from '../ExpenseForm/ExpenseForm';
+import { v4 as uuid } from 'uuid';
 
 const NewExpense = () => {
+    const unique_id = uuid().slice(0,8);
     const saveExpenseDataHandler = enteredExpenseData => {
         const expenseData = {
             ...enteredExpenseData,
-            id: Math.random().toString()
+            id: unique_id
         }
         console.log(expenseData)
     }
