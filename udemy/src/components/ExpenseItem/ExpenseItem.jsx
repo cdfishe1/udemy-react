fd842f6688b4f772b668bc76cd4fc2c835501891
Charles Fisher <charles.fisher@americaneagle.com>
2022-08-01 09:51:49 -0500

    Remove click handler from expense item.

--- udemy/src/components/ExpenseItem/ExpenseItem.jsx
+++ udemy/src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,26 +1,16 @@
-import { useState } from 'react';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import Card from '../Card/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React');
-
-  const buttonClickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
-  } 
-
   return (
     <>
       <Card className='expense-item'>
         <ExpenseDate date={props.date} />
         <div className='expense-item__description'>
-          <h2>{title}</h2>
+          <h2>{props.title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
         </div>
-        <button onClick={buttonClickHandler}>Change Title</button>
       </Card>
     </>
   );
